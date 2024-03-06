a9b84162dde1452c161704a5e6a76a53ebb80027
Mosubmohammed <mosup.050@gmail.com>
2024-03-06 16:44:38 +0300

    final update

--- CrmWebsite/urls.py
+++ CrmWebsite/urls.py
@@ -7,6 +7,12 @@ urlpatterns = [
     path('logout/',logout_user, name='logout'),
     path('register/',register, name='register'),
     path('record/<int:pk>',customer_record, name='record'),
+    path('delete_record/<int:pk>',delete_record, name='delete_record'),
+    path('add_record/',add_record, name='add_record'),
+    path('update_record/<int:pk>',update_record, name='update_record'),
+
+    
+    
     
     
     
--- CrmWebsite/views.py
+++ CrmWebsite/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm,AddRecordForm
 from .models import *
 # Create your views here.
 
@@ -62,4 +62,44 @@ def customer_record(request,pk):
         messages.success(request,"You must be logged in")
         return redirect('home')
 
+
+def delete_record(request,pk):
+    if request.user.is_authenticated:
     
+        delete_it=Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request,"Record deleted successfully!!")
+        return redirect('home')
+    else:
+        messages.success(request,"You must be logged in")
+        return redirect('home')
+
+    
+def add_record(request):
+    form=AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method=="POST":
+            if form.is_valid():
+                form.save()
+                messages.success(request,"Record added successfully!!")
+                return redirect('home')
+        return render(request,'add_record.html',{'form':form})
+    else:
+        messages.success(request,"You Must login to add record")
+        return redirect('home')
+    
+    
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record=Record.objects.get(id=pk)
+        
+        form=AddRecordForm(request.POST or None, instance=current_record)
+        # if request.method=="POST":
+        if form.is_valid():
+                form.save()
+                messages.success(request,"Record updated successfully!!")
+                return redirect('home')
+        return render(request,'update_record.html',{'form':form})
+    else:
+        messages.success(request,"You must be logged in")
+        return redirect('home')
